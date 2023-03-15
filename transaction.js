4dcd77a4566b3089cb8e2a725f772a319a3f2946
RakhaMA <123190109@student.upnyk.ac.id>
2023-03-15 13:35:36 +0800

    Add CreditTerm

--- transaction.js
+++ transaction.js
@@ -1,72 +1,51 @@
 const readline = require('readline-sync');
 
-function calculatePrice(price, discountPercent, taxPercent) {
-  let discountApplied = false;
-  let discountAmount = 0;
-  let discountedPrice = price;
+function purchaseBook(stock, purchased, creditTerm) {
+  const TAX_RATE = 0.1; // 10% tax rate
 
-  if (price > 50000 && discountPercent > 0) {
-    discountAmount = price * (discountPercent / 100);
-    discountedPrice = price - discountAmount;
-    discountApplied = true;
-  }
-
-  let taxAmount = discountedPrice * (taxPercent / 100);
-  let priceAfterTax = discountedPrice + taxAmount;
-
-  return {
-    price: price,
-    discountApplied: discountApplied,
-    discountPercent: discountPercent,
-    discountAmount: discountAmount,
-    discountedPrice: discountedPrice,
-    taxPercent: taxPercent,
-    taxAmount: taxAmount,
-    priceAfterTax: priceAfterTax
-  };
-}
-
-function purchaseBook(stock, purchased) {
+  console.log("==============================================================");
   let bookTitle = readline.question("Enter book title: ");
   let author = readline.question("Enter author name: ");
   let price = parseFloat(readline.question("Enter book price: "));
-  let discountPercent = parseFloat(readline.question("Enter discount percentage (optional): "));
+  let discountPercent = parseFloat(readline.question("Enter discount percentage: "));
   let taxPercent = parseFloat(readline.question("Enter tax percentage: "));
+  console.log("==============================================================");
 
-  let priceDetails = calculatePrice(price, discountPercent, taxPercent);
-
-  let totalPrice = 0;
-  let outOfStock = false;
-
-  for (let i = 0; i < purchased; i++) {
-    if (stock <= 0) {
-      outOfStock = true;
-      break;
-    }
-    totalPrice += priceDetails.priceAfterTax;
-    stock--;
+  let discountAmount = 0;
+  if (price > 50000) {
+    discountAmount = price * (discountPercent / 100);
   }
 
+  let discountedPrice = price - discountAmount;
+  let taxAmount = discountedPrice * (taxPercent / 100);
+  let priceAfterTax = discountedPrice + taxAmount;
+
+  if (purchased + creditTerm > stock) {
+    console.log(`Only ${stock - purchased} book(s) left in stock. Cannot purchase more than that.`);
+    return;
+  }
+
+  let totalPrice = 0;
+  let termPrice = priceAfterTax / creditTerm;
+  let dueDates = [];
+  for (let i = 0; i < creditTerm; i++) {
+    totalPrice += termPrice;
+    dueDates.push(`Due ${i + 1} month(s) after purchase`, totalPrice.toFixed(2));
+  }
+
+  console.log("============================ OUTPUT ==================================");
   console.log("Book title:", bookTitle);
   console.log("Author:", author);
   console.log("Price:", price.toFixed(2));
-  
-  if (priceDetails.discountApplied) {
-    console.log("Discount:", priceDetails.discountPercent + "%");
-    console.log("Discount amount:", priceDetails.discountAmount.toFixed(2));
-    console.log("Price after discount:", priceDetails.discountedPrice.toFixed(2));
-  }
-  
-  console.log("Tax:", priceDetails.taxPercent + "%");
-  console.log("Tax amount:", priceDetails.taxAmount.toFixed(2));
-  console.log("Total price:", totalPrice.toFixed(2));
-  
-  if (outOfStock) {
-    console.log("Out of stock!");
-  } else {
-    console.log("Remaining stock:", stock);
-  }
+  console.log("Discount:", discountPercent + "%");
+  console.log("Discount amount:", discountAmount.toFixed(2));
+  console.log("Price after discount:", discountedPrice.toFixed(2));
+  console.log("Tax:", taxPercent + "%");
+  console.log("Tax amount:", taxAmount.toFixed(2));
+  console.log("Price after tax:", priceAfterTax.toFixed(2));
+  console.log(`Total price for ${creditTerm} months:`, totalPrice.toFixed(2));
+  console.log("Credit due dates:", dueDates);
 }
 
-// Example usage
-purchaseBook(5, 3);
+// Example usage:
+purchaseBook(10, 2, 4); // Purchase 2 books with a credit term of 4 months, when 10 books are in stock
